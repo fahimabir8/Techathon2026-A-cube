c0fd0c1765785faf29d2fa4fa8e444df6eeaecc2
abir <iamabir444@gmail.com>
2026-07-04 16:13:47 +0600

    vercel

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ Usage:
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("backend.main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("backend.main:app", host="0.0.0.0", port=8000, reload=True)
